a4d62c61fd89ecf743271fe4f8c18df783e1a92c
Alyssa Nicoll <alyssaamnell@gmail.com>
2021-12-15 12:20:55 -0600

    add content for days 2-10

--- WindowContent_Data.cs
+++ WindowContent_Data.cs
@@ -3,43 +3,78 @@
 public partial class WindowContent
 {
 
-	public static readonly WindowContent[] Days = new WindowContent[]
+  public static readonly WindowContent[] Days = new WindowContent[]
 {
-			new WindowContent
-			{
-				Id = 1,
-				Hyperlink = "https://www.csadvent.christmas/",
-				HyperlinkText = "Day 1 of Thankmas",
-				WindowText = "On the first day of our Thankmas calendar you will find... one handy resource! Today's knowledge nugget is for those deep into the C# world but also those who are curious to learn more about it and possibly venture into it. It's the awesome C# Advent put together by Matthew D. Groves. Enjoy a full month's worth of insightful content: https://www.csadvent.christmas/"
-			},
-			new WindowContent
-			{
-				Id = 2,
-				Hyperlink = "https://telerik.com",
-				HyperlinkText = "Get your license today!",
-				WindowText = "On the second day of Thankmas my Developer Advocate gave to me.... 1 Telerik Blazor license"
-			},
-			new WindowContent
-			{
-				Id = 3,
-				Hyperlink = "https://telerik.com",
-				HyperlinkText = "Get your license today!",
-				WindowText = "On the third day of Thankmas my Developer Advocate gave to me.... 1 Telerik Blazor license"
-			},
-			new WindowContent
-			{
-				Id = 4,
-				Hyperlink = "https://telerik.com",
-				HyperlinkText = "Get your license today!",
-				WindowText = "On the fourth day of Thankmas my Developer Advocate gave to me.... 1 Telerik Blazor license"
-			},
-			new WindowContent
-			{
-				Id = 5,
-				Hyperlink = "https://telerik.com",
-				HyperlinkText = "Get your license today!",
-				WindowText = "On the fifth day of Thankmas my Developer Advocate gave to me.... 1 Telerik Blazor license"
-			}
+      new WindowContent
+      {
+        Id = 1,
+        Hyperlink = "https://www.csadvent.christmas/",
+        HyperlinkText = "Day 1 of Thankmas",
+        WindowText = "On the first day of our Thankmas calendar you will find... one handy resource! Today's knowledge nugget is for those deep into the C# world but also those who are curious to learn more about it and possibly venture into it. It's the awesome C# Advent put together by Matthew D. Groves. Enjoy a full month's worth of insightful content:"
+      },
+      new WindowContent
+      {
+        Id = 2,
+        Hyperlink = "https://candybox2.github.io/",
+        HyperlinkText = "Candy Box ASCII Game",
+        WindowText = "On the second day of our Thankmas calendar you will find... one super fun game! Be ware: it's so funny, it can be addictive. So play mindfully and EAT THE CANDIES:"
+      },
+      new WindowContent
+      {
+        Id = 3,
+        Hyperlink = "https://docs.google.com/forms/d/e/1FAIpQLSecM9Rjn9_J-tGdmgJkbi_eJf57ihoUn2DAcHZ4cp_vbclHAw/viewform",
+        HyperlinkText = "100$ AMAZON GIFTCARD DRAWING!!!",
+        WindowText = "On the third day of our Thankmas calendar you will find... a chance to win a special prize. We're doing a random draw in the first week of January to give away one 100$ Amazon Gift Card to one lucky person, who has completed today's challenge."
+      },
+      new WindowContent
+      {
+        Id = 4,
+        Hyperlink = "https://www.dotnetmauipodcast.com/",
+        HyperlinkText = ".NET MAUI podcast",
+        WindowText = "On the fourth day of our Thankmas calendar you will find... one more handy resource! Today we urge you to check out the .NET MAUI podcast. It's the place to go to if you'd like to learn about the technology  & find out the latest updates with it. It is hosted by the awesome folks from the .NET MAUI team over at Microsoft:"
+      },
+      new WindowContent
+      {
+        Id = 5,
+        Hyperlink = "https://colorhunt.co/",
+        HyperlinkText = "Color Hunt",
+        WindowText = "On the fifth day of our Thankmas calendar you will find... one inspiring resource! If you're anything like us, creative ruts are something that happens every once in a while. Especially when it comes to colors, combinations and theming. So today we share the inspiring Color Hunt, where you can find interesting color palettes for your apps, designs, art & anything else you need color for."
+      },
+      new WindowContent
+      {
+        Id = 6,
+        Hyperlink = "https://docs.google.com/forms/d/e/1FAIpQLSeleVN-LwnPjpiLsj_BFWjkk1_Mksm1QcwBh8kFv3SglL7v9A/viewform",
+        HyperlinkText = "100$ AMAZON GIFTCARD DRAWING!!!",
+        WindowText = "On the sixth day of our Thankmas calendar you will find... a chance to win a special prize. We're doing a random draw in the first week of January to give away one 100$ Amazon Gift Card to one lucky person, who has completed today's challenge."
+      },
+      new WindowContent
+      {
+        Id = 7,
+        Hyperlink = "https://www.youtube.com/c/AngularAirPodcast",
+        HyperlinkText = "Angular Air Podcast",
+        WindowText = "On the seventh day of our Thankmas calendar you will find... one insightful & fun podcast! If you're curious about Angular or need a handy resource to keep up with all the latest in the technology, we have the one for you. The AngularAir podcast has it all - tips, tricks, news & wonderful hosts. Check it out:"
+      },
+      new WindowContent
+      {
+        Id = 8,
+        Hyperlink = "https://blazorrepl.telerik.com/",
+        HyperlinkText = "Blazor REPL",
+        WindowText = "On the eight day of our Thankmas calendar you will find... one easy way to share & run your Blazor code in the browser! No need for more introductions, just go and see for yourself:"
+      },
+      new WindowContent
+      {
+        Id = 9,
+        Hyperlink = "https://docs.google.com/forms/d/e/1FAIpQLSfGvunaWjLukaFrqLT8tRf456FG3U6HP65kGFZszn_6DQx0Pw/viewform",
+        HyperlinkText = "100$ AMAZON GIFTCARD DRAWING!!!",
+        WindowText = "On the ninth day of our Thankmas calendar you will find... a chance to win a special prize. We're doing a random draw in the first week of January to give away one 100$ Amazon Gift Card to one lucky person, who has completed today's challenge."
+      },
+      new WindowContent
+      {
+        Id = 10,
+        Hyperlink = "https://kahoot.it/challenge/05343098",
+        HyperlinkText = "Thankmas Tech Quiz",
+        WindowText = "On the tenth day of our Thankmas calendar you will find... one fun Thankmas tech quiz! Have you ever played Kahoot? We have prepared a quiz we think you'll enjoy:"
+      }
 };
 
 
